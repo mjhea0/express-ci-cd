170a6d6f69f01b5e89936a8f5408e93d306b47a8
Michael Herman <hermanmu@gmail.com>
2016-09-20 10:38:48 -0600

    init

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+const databaseName = 'express_ci_cd';
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${databaseName}`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
+  test: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${databaseName}_test`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  }
+};
